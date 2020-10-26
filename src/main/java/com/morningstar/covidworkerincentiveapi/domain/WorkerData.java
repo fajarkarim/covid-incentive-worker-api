b8792f561615ede4d57558d01f4c861541333cac
Fajar Karim <fajar.karim@btpn.com>
2020-10-26 16:35:49 +0700

    [Fajar]remove spring devtools and unused business entity

--- src/main/java/com/morningstar/covidworkerincentiveapi/domain/WorkerData.java
+++
@@ -1,9 +0,0 @@
-package com.morningstar.covidworkerincentiveapi.domain;
-
-public class WorkerData {
-    String nik;
-    String fullName;
-    String address;
-    String occupation;
-    Integer salaryPerMonth;
-}
