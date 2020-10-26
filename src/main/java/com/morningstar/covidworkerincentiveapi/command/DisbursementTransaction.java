9f52944ad0a644bc5aacdf681c9e47197cb22cc5
Fajar Karim <fajar.karim@btpn.com>
2020-10-26 16:41:05 +0700

    [Fajar] create upload event

--- src/main/java/com/morningstar/covidworkerincentiveapi/command/DisbursementTransaction.java
+++ src/main/java/com/morningstar/covidworkerincentiveapi/command/DisbursementTransaction.java
@@ -4,6 +4,7 @@ import com.morningstar.covidworkerincentiveapi.coreapi.UploadWorkersDataCmd;
 import com.morningstar.covidworkerincentiveapi.coreapi.WorkersDataUploadedEvt;
 import java.util.UUID;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -12,15 +13,21 @@ import org.axonframework.spring.stereotype.Aggregate;
 public class DisbursementTransaction {
     @AggregateIdentifier
     private UUID transactionId;
-    boolean disbursed;
+    private boolean disbursed;
 
     public DisbursementTransaction() {
 
     }
 
     @CommandHandler
-    public void handle(UploadWorkersDataCmd uploadWorkersDataCmd) {
+    public DisbursementTransaction(UploadWorkersDataCmd uploadWorkersDataCmd) {
         AggregateLifecycle.apply(new WorkersDataUploadedEvt(uploadWorkersDataCmd.getTransactionId(),
             uploadWorkersDataCmd.getWorkersData()));
+    }
+
+    @EventSourcingHandler
+    public void on(WorkersDataUploadedEvt workersDataUploadedEvt) {
+        transactionId = workersDataUploadedEvt.getTransactionId();
+        disbursed = false;
     }
 }
