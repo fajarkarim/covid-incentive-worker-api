33ef89b588d61e21c4b311296204c8cce014d7fe
Fajar Karim <fajar.karim@btpn.com>
2020-10-26 10:49:21 +0700

    init

---
+++ src/main/java/com/morningstar/covidworkerincentiveapi/CovidWorkerIncentiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.morningstar.covidworkerincentiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidWorkerIncentiveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidWorkerIncentiveApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morningstar/covidworkerincentiveapi/CovidWorkerIncentiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morningstar.covidworkerincentiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidWorkerIncentiveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
